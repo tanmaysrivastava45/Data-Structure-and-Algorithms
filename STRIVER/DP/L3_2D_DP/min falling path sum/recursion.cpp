f2fad476e01b62fbce7eda057213e52702758416
tanmaysrivastava45 <tanmaysrivastava9868@gmail.com>
2025-06-28 18:51:33 +0530

    LECTURE 3

---
+++ STRIVER/DP/L3_2D_DP/min falling path sum/recursion.cpp
@@ -0,0 +1,52 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to recursively find the maximum path sum for a given cell
+int getMaxUtil(int i, int j, int m, vector<vector<int>> &matrix) {
+    // Base Conditions
+    if (j < 0 || j >= m)
+        return -1e9; // A very large negative value to represent an invalid path
+    if (i == 0)
+        return matrix[0][j]; // The maximum path sum for the top row is the value itself
+    
+    
+    // Calculate the maximum path sum by considering three possible directions: up, left diagonal, and right diagonal
+    int up = matrix[i][j] + getMaxUtil(i - 1, j, m, matrix);
+    int leftDiagonal = matrix[i][j] + getMaxUtil(i - 1, j - 1, m, matrix);
+    int rightDiagonal = matrix[i][j] + getMaxUtil(i - 1, j + 1, m, matrix);
+    
+    // Store the maximum of the three paths in dp
+    return max(up, max(leftDiagonal, rightDiagonal));
+}
+
+// Function to find the maximum path sum in the given matrix
+int getMaxPathSum(vector<vector<int>> &matrix) {
+    int n = matrix.size(); // Number of rows in the matrix
+    int m = matrix[0].size(); // Number of columns in the matrix
+    
+    
+    int maxi = INT_MIN; // Initialize the maximum path sum to a very small value
+    
+    // Iterate through each cell in the first row to find the maximum path sum starting from each of them
+    for (int j = 0; j < m; j++) {
+        int ans = getMaxUtil(n - 1, j, m, matrix); // Calculate the maximum path sum for the last row
+        maxi = max(maxi, ans); // Update the maximum path sum if a larger one is found
+    }
+    
+    return maxi; // Return the maximum path sum
+}
+
+int main() {
+    // Define the matrix as a 2D vector
+    vector<vector<int>> matrix{{1, 2, 10, 4},
+                                {100, 3, 2, 1},
+                                {1, 1, 20, 2},
+                                {1, 2, 2, 1}};
+    
+    // Call the getMaxPathSum function and print the result
+    cout << getMaxPathSum(matrix);
+
+    return 0;
+}
+
